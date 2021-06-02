0d6f02e4e8afbd6399e1441867bce8cc73e7a9a8
Ana Višnevskaja <anavisnia@gmail.com>
2021-06-02 16:56:43 +0300

    Add project files.

---
+++ WARestfulAPI/Data/DataService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WARestfulAPI.Modules;
+
+namespace WARestfulAPI.Data
+{
+    public class DataService
+    {
+        public List<Fruit> Fruits { get; set; }
+
+        public List<Tableware> Tablewares { get; set; }
+
+        public List<Vagetable> Vagetables { get; set; }
+
+        public DataService()
+        {
+            Fruits = new List<Fruit>();
+            Tablewares = new List<Tableware>();
+            Vagetables = new List<Vagetable>();
+        }
+    }
+}
