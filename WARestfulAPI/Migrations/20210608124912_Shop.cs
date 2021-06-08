24b557d419d50b160ad1ddefe03f003641c5d170
anavisnia <anavisnia@gmail.com>
2021-06-08 16:00:02 +0300

    added shop foreig key

---
+++ WARestfulAPI/Migrations/20210608124912_Shop.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WARestfulAPI.Migrations
+{
+    public partial class Shop : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ShopId",
+                table: "Products",
+                column: "ShopId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Shops_ShopId",
+                table: "Products",
+                column: "ShopId",
+                principalTable: "Shops",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Shops_ShopId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_ShopId",
+                table: "Products");
+        }
+    }
+}
--- WARestfulAPI/Modules/Base/Product.cs
+++ WARestfulAPI/Modules/Base/Product.cs
@@ -16,6 +16,7 @@ namespace WARestfulAPI.Modules.Base
 
         public int Quantity { get; set; }
 
+        public Shop shop { get; set; }
         public int ShopId { get; set; }
 
         public bool Deleted { get; set; } = false;
